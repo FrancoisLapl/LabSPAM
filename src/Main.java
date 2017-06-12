87e64af8e4a75799bf535af8c28486aca4c57424
Frédéric-Simon Mimeault <frederic.simon.mimeault@gmail.com>
2017-06-12 15:16:45 -0400

    Add experimentations results

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String filePath = "spamdata-testnolabel-E16.arff";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (Exception e){
+            // Swallow that shit
+        }
     }
+
+    private static void PredictBest(){}
+    private static void PredictWorst(){}
 }
